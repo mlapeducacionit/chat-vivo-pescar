45a1931ecdbfbdcb535cef2ba94b844e82c87186
Maxiliano Principe <mlapeducacionit@gmail.com>
2025-08-05 16:50:05 -0300

    estadisticas

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ const variablesEntorno = {
     public_directory: process.env.PUBLIC_DIR
 }
 
+setInterval(() => {
+    const usage = process.memoryUsage();
+    console.log({
+      rss: (usage.rss / 1024 / 1024).toFixed(2) + ' MB',
+      heapTotal: (usage.heapTotal / 1024 / 1024).toFixed(2) + ' MB',
+      heapUsed: (usage.heapUsed / 1024 / 1024).toFixed(2) + ' MB',
+      external: (usage.external / 1024 / 1024).toFixed(2) + ' MB',
+    });
+}, 10000);
 
 
 const mensajes = [
